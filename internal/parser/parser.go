2babbae89321acb6f8a146e368478e4494116685
Nicolas Hampton <nicolasjhampton@gmail.com>
2018-05-06 10:07:49 -0700

    Adding error messages

--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -1,25 +1,40 @@
 package parser
 
 import (
+	"fmt"
 	"github.com/nicolasjhampton/simple_interpreter/internal/ast"
 	"github.com/nicolasjhampton/simple_interpreter/internal/lexer"
 	"github.com/nicolasjhampton/simple_interpreter/internal/token"
 )
 
 type Parser struct {
-	l *lexer.Lexer
-	curToken token.Token
+	l         *lexer.Lexer
+	errors    []string
+	curToken  token.Token
 	peekToken token.Token
 }
 
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{
+		l:      l,
+		errors: []string{},
+	}
 
 	// Read two tokens, so curToken and peekToken are both set
 	p.nextToken()
 	p.nextToken()
 
 	return p
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead",
+		t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
 }
 
 func (p *Parser) nextToken() {
@@ -31,7 +46,7 @@ func (p *Parser) ParseProgram() *ast.Program {
 	program := &ast.Program{}
 	program.Statements = []ast.Statement{}
 
-	for p.curToken.Type != token.EOF {
+	for !p.curTokenIs(token.EOF) {
 		stmt := p.parseStatement()
 		if stmt != nil {
 			program.Statements = append(program.Statements, stmt)
@@ -85,10 +100,12 @@ func (p *Parser) peekTokenIs(t token.TokenType) bool {
 }
 
 func (p *Parser) expectPeek(t token.TokenType) bool {
-	if p.peekTokenIs(t) {
+	isValid := p.peekTokenIs(t)
+	switch isValid {
+	case true:
 		p.nextToken()
-		return true
-	} else {
-		return false
+	default:
+		p.peekError(t)
 	}
+	return isValid
 }
